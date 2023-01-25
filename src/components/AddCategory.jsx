f2f9f0e0985ae38ed911fec25ac94d49e2711a74
Alejandro Rios <alejo.950723@gmail.com>
2023-01-25 07:43:53 -0500

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export const AddCategory = ( {onNewCategory} ) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    // event.target.input
+    const onInputChange = ( {target} ) => { // desestructuracion de target
+
+        // console.log( target.value );
+        setInputValue( target.value );
+    }
+
+    const onSubmit = ( event ) => {
+
+        event.preventDefault();
+
+        if( inputValue.trim().length <= 1 ) return;
+        // setCategories( categories => [...categories, inputValue ] );
+        onNewCategory( inputValue.trim() );
+        setInputValue('');
+    }
+
+  return (
+    // <form onSubmit={ (event) => onSubmit(event) }>
+    <form onSubmit={ onSubmit }>
+        <input
+            type='text'
+            placeholder="Buscar Gif"
+            onChange={ onInputChange } // recibe 'event' implicitamente como argumento  
+            value={inputValue}
+        />
+    </form>
+  );
+}
