b046ad67779d969a3b854e7a286cc1d615b6191e
Alejandro Rios <alejo.950723@gmail.com>
2023-01-26 23:06:35 -0500

    update final

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { AddCategory, GifGrid } from "./components";
-import { useFetchGifs } from "./Hooks/useFetchGifs";
 
 export const GifExpertApp = () => {
 
@@ -8,31 +7,28 @@ export const GifExpertApp = () => {
 
     const onAddCategory = ( newCategory ) => {
         if( categories.includes(newCategory) ) return;
-
-        setCategories([ ...categories, newCategory ]);  
+        setCategories([ newCategory, ...categories ]);  
     }
 
     return (
         <>
-            <div className="background"></div>
-
             <h1>Buscador de Gifs variados</h1>
 
             <AddCategory 
-                setCategories={ setCategories }
                 onNewCategory={ value => onAddCategory(value) }
-                />
+            />
+
+            { categories < 1 ? <h2>Los Gifs se irán desplegando en la parte inferior...</h2> : '' }
+
             {
-                categories < 1 ? <h2>Los Gifs se irán desplegando en la parte inferior...</h2> : ''
+                categories.map( category => (
+                        <GifGrid 
+                            key={ category } 
+                            category={ category }
+                        />
+                    ))
             }
-        {
-            categories.map( category => (
-                    <GifGrid 
-                        key={ category } 
-                        category={ category }
-                    />
-                ))
-        }
+
         </>
     );
 }
